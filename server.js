23fe90f9299d635e8f487e37a4701459b649bad2
HarleyVader <urukkai@outlook.com>
2024-05-27 20:36:28 +0200

    1.3.5 /bambi

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/bambi', (req, res) => {
     res.send('Hello Bambi');
     });
 
-server.listen(port, 'localhost', () => {
+server.listen(port, '127.0.0.1', () => {
     console.log(`Server is running on https://bambisleep.chat:${port}`);
 }).on('error', (err) => {
     console.error('Error occurred while starting the server:', err);
