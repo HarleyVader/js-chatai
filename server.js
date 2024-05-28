a181a9b59dba9a8f4f37bf333eb2cf7fa8c93e72
HarleyVader <urukkai@outlook.com>
2024-05-28 03:36:49 +0200

    1.4.7

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ io.on('connection', (socket) => {
             console.log('Received message:', message);
 
             // Read the pre-prompt content from the file
-            const templatePath = path.join(__dirname, '/public/templates/bambisleep-rp.json');
+            const templatePath = path.join(__dirname, '/public/templates/bambisleep-rp_0.0.1.json');
 
             if (!fs.existsSync(templatePath)) {
                 console.error('Template file does not exist.');
